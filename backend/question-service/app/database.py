905e7a9dcf1e2f6b679851915224621bd196648b
Ponharish <subramanian.ponharish@u.nus.edu>
2026-03-06 00:00:09 +0800

    update database.py
    update logic to fetch config file from env

--- backend/question-service/app/database.py
+++ backend/question-service/app/database.py
@@ -1,9 +1,17 @@
+import os
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+load_dotenv()
+
+key_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
 # Initialize Firestore
-# Note: Ensure the JSON key is in your project root or set via ENV
-cred = credentials.Certificate("firebase-questionservice.json")
-firebase_admin.initialize_app(cred)
+if os.path.exists(key_path):
+    cred = credentials.Certificate(key_path)
+    firebase_admin.initialize_app(cred)
+else:
+    firebase_admin.initialize_app()
 
 db = firestore.client()
