36167ff8c1ba42a17e6f85bdbde681199540bdf5
Kannan171 <kannanannamalai100@gmail.com>
2026-03-17 17:46:01 +0800

    Update matching service to allow multiple topics and difficulties

--- backend/matching-service/app/schemas.py
+++ backend/matching-service/app/schemas.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel, Field
+from typing import List
 
-class MatchRequest(BaseModel):
-    topic: str = Field(..., description="Question topic (e.g., Arrays, Strings)")
-    difficulty: str = Field(..., description="Difficulty level (Easy, Medium, Hard)")
+class FindPairRequest(BaseModel):
+    topic_options: List[str] = Field(..., description="List of topics (e.g., ['Arrays', 'Strings'])", min_items=1)
+    difficulty_options: List[str] = Field(..., description="List of difficulties (e.g., ['Easy', 'Medium'])", min_items=1)
 
 class MatchResponse(BaseModel):
     message: str
-    queue_name: str
-    user_id: str
--- backend/matching-service/app/worker.py
+++ backend/matching-service/app/worker.py
@@ -1,10 +1,11 @@
 import asyncio
 import json
+import time
 from app import database
 
 async def match_worker():
-    """Background task that sweeps queues and publishes match data to the API Gateway."""
-    print("Event-driven background worker started...")
+    """Sweeper that handles 60-second timeouts."""
+    print("Timeout Sweeper Worker started...")
     while True:
         try:
             # Safety check: ensure Redis is connected before sweeping
@@ -12,52 +13,24 @@ async def match_worker():
                 await asyncio.sleep(1)
                 continue
 
-            cursor = 0
-            while True:
-                cursor, keys = await database.redis_match.scan(cursor=cursor, match="queue:*")
-                for key in keys:
-                    users = await database.redis_match.lrange(key, 0, -1)
-                    for uid in users:
-                        # If timeout
-                        if not await database.redis_match.exists(f"active_user:{uid}"):
-                            await database.redis_match.lrem(key, 0, uid)
-                            # Timeout Event
-                            timeout_payload = json.dumps({
-                                "event": "timeout",
-                                "user_id": uid
-                            })
-                            await database.redis_pubsub.publish(f"match_events:{uid}", timeout_payload)
-                            print(f"Sent timeout event for {uid}")
+            now = int(time.time())
+            # Users with timeout_tracket set 
+            timed_out_users = await database.redis_match.zrangebyscore("timeout_tracker", 0, now)
 
-                    while await database.redis_match.llen(key) >= 2:
-                        user1 = await database.redis_match.lpop(key)
-                        user2 = await database.redis_match.lpop(key)
-                        
-                        if user1 and user2:
-                            _, topic, difficulty = key.split(":")
-                            
-                            # Cleanup active status so users can queue again in the future
-                            await database.redis_match.delete(f"active_user:{user1}")
-                            await database.redis_match.delete(f"active_user:{user2}")
-                            
-                            # Create payload 
-                            match_payload = {
-                                "user1": user1,
-                                "user2": user2,
-                                "topic": topic,
-                                "difficulty": difficulty
-                            }
-                            json_data = json.dumps(match_payload)
-                            
-                            # Broadcast to Event Bus
-                            await database.redis_pubsub.publish(f"match_events:{user1}", json_data)
-                            await database.redis_pubsub.publish(f"match_events:{user2}", json_data)
-                            
-                            print(f"Match Broadcasted! {user1} & {user2} for {topic} ({difficulty})")
-                if int(cursor) == 0:
-                        break
-                            
+            for uid in timed_out_users:
+                # Remove active status and timeout tracker
+                async with database.redis_match.pipeline() as pipe:
+                    pipe.delete(f"active_user:{uid}")
+                    pipe.zrem("timeout_tracker", uid)
+                    await pipe.execute()
+
+                timeout_payload = json.dumps({
+                    "event": "timeout",
+                    "user_id": uid
+                })
+                await database.redis_pubsub.publish(f"match_events:{uid}", timeout_payload) # Timeout event
+                
+                print(f"Sent timeout event for {uid}. User is no longer active in queue.")       
         except Exception as e:
             print(f"Worker error: {str(e)}")
-        
-        await asyncio.sleep(1)
+        await asyncio.sleep(1) # Check every second
