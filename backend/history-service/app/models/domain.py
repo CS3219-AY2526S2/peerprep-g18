52d67416e07308358b6cd33c3b67c3b3cea8cd46
Ponharish <subramanian.ponharish@u.nus.edu>
2026-04-09 22:58:06 +0800

    Update History service domains

--- backend/history-service/app/models/domain.py
+++ backend/history-service/app/models/domain.py
@@ -1,20 +1,19 @@
 from pydantic import BaseModel, Field, field_validator, ConfigDict
-from typing import Optional, List
+from typing import Any, Optional, List
 from datetime import datetime, timezone
 
 class HistoryBase(BaseModel):
     sessionId: str
     user1Id: str = Field(..., alias="user1_id")
     user2Id: str = Field(..., alias="user2_id")
-    questionId: int
+    questionId: int 
     
     submittedBy: Optional[str] = None
     
-    # The problem that was attempted
-    title: str = Field(..., min_length=3, max_length=150)
-    topic: str
-    difficulty: str 
-    statement: str
+    title: Optional[str] = Field(None, min_length=3, max_length=150)
+    topic: Optional[str] = None
+    difficulty: Optional[str] = None 
+    statement: Optional[str] = None
     examples: List[str] = []
     constraints: List[str] = []
     hints: List[str] = []
@@ -22,28 +21,28 @@ class HistoryBase(BaseModel):
     # The user's actual work. NOT the initial template!
     codeSubmitted: Optional[str] = Field(None, alias="finalCode")
 
-    startedAt: Optional[datetime] = None
+    startedAt: datetime #This one is required
     endedAt: Optional[datetime] = None
 
     model_config = ConfigDict(populate_by_name=True, extra="ignore")
 
     @field_validator('title', 'topic', 'difficulty', mode='after')
     @classmethod
-    def format_to_title_case(cls, v: str) -> str:
+    def format_to_title_case(cls, v: Optional[str]) -> Optional[str]:
         if v:
             return v.strip().title()
         return v
 
     @field_validator('startedAt', 'endedAt', mode='before')
     @classmethod
-    def transform_unix(cls, v):
+    def transform_unix(cls, v: Any):
         if isinstance(v, (int, float)):
             return datetime.fromtimestamp(v / 1000 if v > 1e12 else v, tz=timezone.utc)
         return v
 
     @field_validator('questionId', mode='before')
     @classmethod
-    def ensure_int(cls, v):
+    def ensure_int(cls, v: Any):
         try:
             return int(v)
         except (ValueError, TypeError):
